f11b9b48e21806ad1db8e202c0362e6590da76bf
Tom Nelson <tkoinaustin@yahoo.com>
2020-03-13 14:10:45 -0500

    Bug fix, behavior of load and save buttons was not stable, sometimes load did not load

--- Pixellate/MainViewController.swift
+++ Pixellate/MainViewController.swift
@@ -11,12 +11,14 @@ import CoreImage
 
 class MainViewController: UIViewController {
 
-    @IBOutlet weak var saveButton: UIButton!
+    @IBOutlet weak var saveButton: UIButton! { didSet {
+        self.saveButton.isEnabled = false
+    }}
     @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var savedImageView: UIImageView!
     
-    private var isDrawing = false
+    private var isDrawing = true
     private var lastPoint: CGPoint!
     private var strokeColor: CGColor = UIColor.black.cgColor
     private var strokes = [Stroke]()
@@ -44,7 +46,7 @@ class MainViewController: UIViewController {
     }
     
     @IBAction func resetAction(_ sender: UIButton) {
-//        self.saveButton.isEnabled = true
+        self.isDrawing = true
         self.savedImageView.image = nil
         self.strokes = [Stroke]()
         self.updateMaskLayer()
@@ -67,8 +69,7 @@ class MainViewController: UIViewController {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        guard !isDrawing else { return }
-        isDrawing = true
+        guard isDrawing else { return }
         guard let touch = touches.first else { return }
         let currentPoint = touch.location(in: self.imageView)
         lastPoint = currentPoint
@@ -88,7 +89,6 @@ class MainViewController: UIViewController {
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard isDrawing else { return }
-        isDrawing = false
         guard let touch = touches.first else { return }
         let currentPoint = touch.location(in: self.imageView)
         let stroke = Stroke(startPoint: lastPoint, endPoint: currentPoint, color: strokeColor)
@@ -143,6 +143,8 @@ class MainViewController: UIViewController {
 
     func save() {
         let start = DispatchTime.now().uptimeNanoseconds
+        guard self.isDrawing else { return }
+        self.isDrawing = false
 
         UIGraphicsBeginImageContextWithOptions(self.imageView.bounds.size, false, UIScreen.main.scale)
         self.imageView.drawHierarchy(in: self.imageView.bounds, afterScreenUpdates: true)
